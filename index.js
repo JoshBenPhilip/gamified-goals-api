245bf7f0fe6bce19018b5185074796b0496ebd55
JoshBenPhilip <joshben4c@gmail.com>
2022-06-10 18:22:30 -0400

    semiworking api

---
+++ index.js
@@ -0,0 +1,19 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { getTasks, createTask, updateTask } = require("./src/tasks");
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.post("/tasks", createTask);
+app.get("/tasks", getTasks);
+app.patch("/tasks/:taskId", updateTask);
+
+app.listen(PORT, () => {
+  console.log("Listening on Port: ", PORT);
+});
