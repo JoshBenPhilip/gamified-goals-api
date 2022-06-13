5e5a0cc39d0ec8ad5d128fa021344c3ab10aced9
JoshBenPhilip <joshben4c@gmail.com>
2022-06-13 09:30:42 -0400

    deployed backend

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.send("Gamified Goals");
+});
+
 // Routes
 app.post("/tasks", createTask);
 app.get("/tasks", getTasks);
